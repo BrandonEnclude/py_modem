10b7c3ffee0fb8dfd5722c8d05da6815eb9cbd03
Brandon <brandon.dematteis@enclude.ie>
2020-05-06 11:03:57 +0100

    added pings to keep connection alive

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ import time
 import os
 import sys
 
-URI = os.environ.get('WEBSOCKET_URI')
-WS_KEY = os.environ.get('WS_KEY')
+URI = os.environ.get('WEBSOCKET_URI', 'ws://localhost:8000/ws/')
+WS_KEY = os.environ.get('WS_KEY', 'ws_key')
 RECONNECT_DELAY = int(os.environ.get('RECONNECT_DELAY', 3))
 
 ssl_context = ssl.SSLContext()
@@ -22,16 +22,26 @@ class App:
         self.URI = URI
         self.sims = None
         self.websocket = None
+        self.stay_connected = True
 
     async def listen(self):
         try:
+            # async with websockets.connect(self.URI, extra_headers=headers) as websocket:
             async with websockets.connect(self.URI, ssl = ssl_context, extra_headers=headers) as websocket:
                 self.websocket = websocket
-                while True:
+                asyncio.ensure_future(self.keep_alive())
+                await self.websocket.send(json.dumps({'id': int(time.time()), 'jsonrpc':'2.0','method':'sms_server.reconnect_done','params':{'status': 'Ok'}}))
+                while self.stay_connected:
                     msg = await websocket.recv()
                     await self._on_message(msg)
-        except websockets.ConnectionClosed as e:
-            await self._teardown_and_rebuild()
+        except Exception as e:
+            print(repr(e), flush=True)
+            await self._tear_down()
+
+    async def keep_alive(self):
+        while self.websocket.open:
+            await self.websocket.send(json.dumps({'id': int(time.time()), 'jsonrpc':'2.0','method':'sms_server.ping','params':{}}))
+            await asyncio.sleep(60)
 
     async def _on_message(self, msg):
         jsonrpc = json.loads(msg)
@@ -40,20 +50,14 @@ class App:
             method = getattr(self, method_name)
             await method(**params)
 
-    async def _teardown_and_rebuild(self, delay = RECONNECT_DELAY):
+    async def _tear_down(self, delay = RECONNECT_DELAY):
         try:
             await self.sims.close_all()
-        except:
+        except AttributeError:
             pass
-        await asyncio.sleep(delay)
-        try:
-            await self.listen()
-        except RecursionError:
-            sys.exit()
         except Exception as e:
-            #TODO: log error
             print(repr(e), flush=True)
-            await self._teardown_and_rebuild()
+        self.stay_connected = False
 
     def _extract_params(self, jsonrpc):
         if jsonrpc.get('result') is None and jsonrpc.get('method') is None:
@@ -79,27 +83,24 @@ class App:
         await self.sims.get_stored_messages()
 
     async def sim_status(self, id = None):
-        id = id if id is not None else int(time.time())
-        await self.websocket.send(json.dumps({'id': id, 'jsonrpc':'2.0','method':'sms_server.broadcast_sim_status','params':{'id': id, 'sims': self.sims.to_dict()}}))
+        await self.websocket.send(json.dumps({'id': int(time.time()), 'jsonrpc':'2.0','method':'sms_server.broadcast_sim_status','params':{'sims': self.sims.to_dict()}}))
 
     async def send_sms(self, id, msg, sim_number, recipient_number):
         try:
             await self.sims.send_sms(id, msg, sim_number, recipient_number)
         except Exception as e:
-            await self.websocket.send(json.dumps({'id': id, 'jsonrpc':'2.0','method':'sms_server.sent_status','params':{'id': id, 'message': f'ERR: {repr(e)}'}}))
+            await self.websocket.send(json.dumps({'id': int(time.time()), 'jsonrpc':'2.0','method':'sms_server.sent_status','params':{'message': f'ERR: {repr(e)}'}}))
         else:
-            await self.websocket.send(json.dumps({'id': id, 'jsonrpc':'2.0','method':'sms_server.sent_status','params':{'id': id, 'message': 'Sent'}}))
+            await self.websocket.send(json.dumps({'id': int(time.time()), 'jsonrpc':'2.0','method':'sms_server.sent_status','params':{'message': 'Sent'}}))
 
     async def delete_stored_sms(self, sim_number, msg_index):
         await self.sims.delete_stored_sms(sim_number, msg_index)
 
-    async def reconnect(self, id):
-        try:
-            await self._teardown_and_rebuild(3)
-        except Exception as e:
-            await self.websocket.send(json.dumps({'id': id, 'jsonrpc':'2.0','method':'sms_server.reconnect_done','params':{'id': id, 'status': repr(e)}}))
-        await self.websocket.send(json.dumps({'id': id, 'jsonrpc':'2.0','method':'sms_server.reconnect_done','params':{'id': id, 'status': 'Ok'}}))
+    async def reconnect(self):
+        await self._tear_down(3)
 
 if __name__ == "__main__":
     app = App(URI)
     asyncio.get_event_loop().run_until_complete(app.listen())
+
+    
