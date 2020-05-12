4573a2e78481e47a22978483a5f2a2625e359248
Brandon <brandon.dematteis@enclude.ie>
2020-05-12 08:42:57 +0100

    create task test

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ class App:
                 while self.stay_connected:
                     msg = await websocket.recv()
                     asyncio.create_task(self._on_message(msg))
-                    await self._on_message(msg)
+                    # await self._on_message(msg)
         except Exception as e:
             logging.error('at %s', 'App.listen', exc_info=e)
             await self._tear_down()
